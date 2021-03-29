117dcc55c1daae1ae3fcccfb8da4be0598a81e44
Carl Benbow <cbenbow@synergisticagility.com>
2021-03-29 06:25:02 -0400

    successful getting the first api

--- assets/js/weather_dashboard.js
+++ assets/js/weather_dashboard.js
@@ -1,10 +1,42 @@
 $(document).ready(function(){
 
+  // $(".searchBtn").val
+
+  
+  // var apiKey = "92082348e1fdf20edfb9752ebc60f8bc"
+
+  // var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q=boston&appid='+ apiKey;
+
+  // var responseText = $('.state-results');
+  
+  // function getApi(requestUrl) {
+  //   fetch(requestUrl)
+  //     .then(function (data) {
+  //       console.log(data);
+  //       // if (response.status === 200) {
+  //       //   // responseText.textContent = 
+  //       // }
+  //       return response.json();
+  //   });
+  // }
+  
+  // getApi(requestUrl);
+  
     
+   
+  var apiKey = "92082348e1fdf20edfb9752ebc60f8bc"
+
+  var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q=boston&appid='+ apiKey;
     
+    fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
     
-    
-    
+    //API Current weather api call 
     
     
     
