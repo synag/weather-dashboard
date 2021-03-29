4bcc71279139574dbaae5d44072a0b09bb33721e
Carl Benbow <cbenbow@synergisticagility.com>
2021-03-29 18:03:57 -0400

    created the five-day API

--- assets/js/weather_dashboard.js
+++ assets/js/weather_dashboard.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 
   
+  var apiKey = "92082348e1fdf20edfb9752ebc60f8bc"
+  var lat = 0
+  var long = 0 
+  var city = "boston"
+
   function getCurrentWeather(){
    
-  var apiKey = "92082348e1fdf20edfb9752ebc60f8bc"
-
-  var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q=boston&appid='+ apiKey;
+  var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ apiKey;
     
     fetch(requestUrl)
   .then(function (response) {
@@ -13,19 +16,61 @@ $(document).ready(function(){
   })
   .then(function (data) {
     console.log(data);
-     $(".todaysDate").text(data.name);
+     $(".state-results").text(data.name);
      $(".weatherIconCurrent").text(data.weather.icon); 
-     $(".temperatureCurrent").text(data.main.temp);
-     $(".humidityCurrent").text(data.main.humidity);
+     $(".temperatureCurrent").text("Temperature: " +data.main.temp +"");
+     $(".humidityCurrent").text("Humidity: "+data.main.humidity+"%");
      $(".uvIndexCurrent").text(data.name);
-     $(".windSpeedCurrent").text(data.wind.speed);
-
-
-
+     $(".windSpeedCurrent").text("Wind Speed: "+data.wind.speed+" MPH");
+     lat = data.coord.lat
+     long = data.coord.lon
+     
   });
     
 }
-    getCurrentWeather()
+
+//UV function to get the UV infomration
+function uvData(){
+
+  var requestUrl ='https://api.openweathermap.org/data/2.5/onecall?lat='+ lat+ 'lon='+long+'&exclude=hourly,daily,minutely&appid=' +apiKey;
+    
+  fetch(requestUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+ 
+  //  $(".uvIndexCurrent").text(data.name);
+  //  $(".windSpeedCurrent").text("Wind Speed: "+data.wind.speed+" MPH");
+
+})
+}
+
+//five day forecast function for
+function fiveDayForecast(){
+
+  var requestUrl ='https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=' +apiKey;
+    
+  fetch(requestUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+  $(".1-day-time").text(data.list[1].dt_txt);
+  $(".1-day-temp").text("Temperature: " +data.list[1].main.temp); 
+  $(".1-day-humidity").text("Humidity: "+data.list[1].main.humidity+"%"); 
+  // $(".1 day-humidity").text(data."Temperature: " +data.main.temp +"");
+
+})
+}
+
+getCurrentWeather()
+fiveDayForecast()
+ uvData()
+
+   
     
     
     
