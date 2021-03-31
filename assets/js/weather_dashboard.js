8049eacd591cd42506c39233ac3b32aad8d500e7
Carl Benbow <cbenbow@synergisticagility.com>
2021-03-31 07:38:11 -0400

    button is linked to api function and it works

--- assets/js/weather_dashboard.js
+++ assets/js/weather_dashboard.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
   var apiKey = "92082348e1fdf20edfb9752ebc60f8bc"
   var lat = 0
   var long = 0 
-  var city = "boston"
+  var city = ""
 
   function getCurrentWeather(){
    
-  var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ apiKey;
+  var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q='+city+ "&units=imperial"
+  +'&appid='+ apiKey;
     
     fetch(requestUrl)
   .then(function (response) {
@@ -50,7 +51,7 @@ function uvData(){
 //five day forecast function for
 function fiveDayForecast(){
 
-  var requestUrl ='https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=' +apiKey;
+  var requestUrl ='https://api.openweathermap.org/data/2.5/forecast?q='+city+'&units=imperial'+'&appid=' +apiKey;
     
   fetch(requestUrl)
 .then(function (response) {
@@ -58,20 +59,60 @@ function fiveDayForecast(){
 })
 .then(function (data) {
   console.log(data);
-  $(".1-day-time").text(data.list[1].dt_txt);
+  var reformatDate1 = (data.list[5].dt_txt);
+  var reformatDate2 = (data.list[13].dt_txt);
+  var reformatDate3 = (data.list[21].dt_txt);
+  var reformatDate4 = (data.list[29].dt_txt);
+  var reformatDate5 = (data.list[37].dt_txt);
+  
+  $(".1-day-time").text(moment(reformatDate1).format("l"))
   $(".1-day-temp").text("Temperature: " +data.list[1].main.temp); 
-  $(".1-day-humidity").text("Humidity: "+data.list[1].main.humidity+"%"); 
-  // $(".1 day-humidity").text(data."Temperature: " +data.main.temp +"");
+  $(".1-day-humidity").text("Humidity: "+data.list[1].main.humidity+"%");
 
+  $(".2-day-time").text(moment(reformatDate2).format("l"))
+  $(".2-day-temp").text("Temperature: " +data.list[2].main.temp); 
+  $(".2-day-humidity").text("Humidity: "+data.list[2].main.humidity+"%"); 
+  
+  $(".3-day-time").text(moment(reformatDate3).format("l"))
+  $(".3-day-temp").text("Temperature: " +data.list[3].main.temp); 
+  $(".3-day-humidity").text("Humidity: "+data.list[3].main.humidity+"%"); 
+
+  $(".4-day-time").text(moment(reformatDate4).format("l"))
+  $(".4-day-temp").text("Temperature: " +data.list[4].main.temp); 
+  $(".4-day-humidity").text("Humidity: "+data.list[4].main.humidity+"%"); 
+ 
+  $(".5-day-time").text(moment(reformatDate5).format("l"))
+  $(".5-day-temp").text("Temperature: " +data.list[5].main.temp); 
+  $(".5-day-humidity").text("Humidity: "+data.list[5].main.humidity+"%"); 
+  
 })
 }
 
+
+
+$(".searchBtn").click(function(event){
+  event.preventDefault();
+  // action goes here!!
+  city = $(".form-control").val()
+  
 getCurrentWeather()
 fiveDayForecast()
- uvData()
+});
+
+// event.preventDefault();
+// // city = event.target.parentElement.parentElement.children[0].value
+//   city = $(".form-control").val()
+// getCurrentWeather()
+// fiveDayForecast()
+
+
+
+// getCurrentWeather()
+// fiveDayForecast()
+//  uvData()
 
    
-    
+
     
     
     
